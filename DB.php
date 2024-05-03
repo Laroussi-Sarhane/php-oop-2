12f7b4c99699ddeb31478f64a5c4ba78ccd85e65
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-03 15:45:14 +0200

    add db

---
+++ DB.php
@@ -0,0 +1,39 @@
+<?php 
+
+$database = [
+    "prodotti" => [
+
+        "giochi" => [
+            [
+                "nome" => "osso di gomma",
+                "tipo" => "osso",
+                "materiale" => "gomma",
+                "peso" => "0.200",
+
+            ],
+            [
+                "nome" => "pallina",
+                "tipo" => "Auto",
+                "materiale" => "plastica" ,
+                "peso" => "0.200",
+
+            ]
+        ],
+
+     "cibo" => [
+            [
+                "tipologia" => "snack",
+                "riferimento" => "Cane",
+                "ingredienti" => ["pesce", "anatra"]
+            ],
+            [
+                "tipologia" => "Croccantini",
+                "riferimento" => "Cane",
+                "ingredienti" => ["salmone", "anatra"]
+            ],
+            
+            
+        ]
+    ]
+];
+?>
