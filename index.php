09a4d2035528948164970b0cdb578cf2580bf06c
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-03 18:53:44 +0200

    add chages

--- index.php
+++ index.php
@@ -5,6 +5,25 @@ require_once __DIR__. '/giochi.php';
 require_once __DIR__. '/prodotti.php';
 require_once __DIR__. '/DB.php';
 
+foreach($prodotti as $type ){
+  $cibo = new Cibo(
+    $prodotti["tipologia"],
+    $prodotti["riferimento"],
+    $prodotti["tipo"],
+
+  )
+}
+// foreach($prodotti as $other ){
+//   $giochi = new Giochi(
+//     $prodotti["nome"],
+//     $prodotti["tipo"],
+//     $prodotti["materiale"],
+//     $prodotti["peso"],
+
+
+//   )
+}
+
 
 
 
