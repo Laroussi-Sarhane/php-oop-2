60105ef3762e3dd81d5dcd781feb0356996a1c0f
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-03 15:58:45 +0200

    add prodotti

---
+++ prodotti.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Prodotti{
+
+  public $nome;
+  public $tipo;
+  public $materiale;
+
+  public function __construct(string $_nome,string $_tipo,string $_materiale)
+  {
+    $this->nome= $_nome;
+    $this->tipo= $_tipo;
+    $this->materiale= $_materiale;
+  }
+
+  public function getProductInfo(){
+    return "$this->nome,$this->tipo,$this->materiale,";
+  }
+
+}
