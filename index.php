3b009406c46025b9416f17f16c8e30dee6a15c39
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-06 16:47:39 +0200

    add

--- index.php
+++ index.php
@@ -5,22 +5,9 @@ require_once __DIR__. '/Cibo.php';
 require_once __DIR__. '/Giochi.php';
 require_once __DIR__. '/DB.php';
 
-$nuovoCibo = [];
-$nuoviGiochi = [];
-
-
-
-
-?>
-
-
-
-
-
-
-
-
-
+$cibo= new Cibo("snack");
+$giochi= new Giochi("0.200");
+$prodotti= new Prodotti ('cane','plastica','')
 
 
 
@@ -30,6 +17,23 @@ $nuoviGiochi = [];
 
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -47,5 +51,33 @@ $nuoviGiochi = [];
 </head>
 <body>
   
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">cibo</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach($cibo as $item): ?>
+    <tr>
+      <th scope="row"><?php echo $item->nome?></th>
+      <td><?php echo $item->tipologia?></td>
+      
+    </tr>
+    <?php endforeach; ?>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+   
+    </tr>
+  </tbody>
+</table>
 </body>
 </html>
