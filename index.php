430657e225b3de3f24cc500167ceddd32d8907a2
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-06 16:35:11 +0200

    ADD

--- index.php
+++ index.php
@@ -1,37 +1,16 @@
 <?php
 
-require_once __DIR__. '/cibo.php';
-require_once __DIR__. '/giochi.php';
+require_once __DIR__. '/Prodotti.php';
+require_once __DIR__. '/Cibo.php';
+require_once __DIR__. '/Giochi.php';
 require_once __DIR__. '/DB.php';
-require_once __DIR__. '/prodotti.php';
 
 $nuovoCibo = [];
 $nuoviGiochi = [];
 
 
 
-foreach ($prodotti as $item => $prodottiItem) {
-  foreach ($prodottiItem as $prodotti) {
-      if ($item === "cibo") {
-         
-          $cibo = new Cibo(
-              $prodotti["nome"],
-              $prodotti["tipo"],
-              $prodotti["materiale"],
-          );
 
-          $nuovoCibo[] = $cibo;
-      } elseif ($category === "toys") {
-          $giochi = new Giochi(
-              $product["tipo"],
-           
-          );
-
-        
-          $formattedToys[] = $toy;
-      }
-  }
-}
 ?>
 
 
